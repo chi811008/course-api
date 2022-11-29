9d6b9ad8041a600e6f9066d7972f97c2eabcbe28
Seraphine_Tsui <chi811008@gmail.com>
2022-11-29 13:58:40 +0800

    feat: model validates, belongs, uniqueness

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+	validates_presence_of :name, :lecturer_name
+	has_many :unit
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,5 @@
 class Unit < ApplicationRecord
+	belongs_to :course
+	validates_presence_of :name, :course_id, :content, :order
+	validates_uniqueness_of :order, :scope => :course_id
 end
